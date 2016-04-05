a2625a32c0667ae622ba509287d5344c936fdaf4
Monica Parra <moni18r@gmail.com>
2016-04-05 14:27:32 -0400

    mutually exclusive playback demo fixed

--- demos/basic/mutually-exclusive/js/main.js
+++ demos/basic/mutually-exclusive/js/main.js
@@ -2,22 +2,18 @@ var player1 = jwplayer("player1");
 var player2 = jwplayer("player2");
 var player3 = jwplayer("player3");
 
+// When a player size is set to a fixed height of 40px, the player will enter audio mode.
+
 player1.setup({
   file: "//content.jwplatform.com/videos/1b02B03R-g8UjtXW6.m4a",
-  height: 40,
-  width: 480
 });
 
 player2.setup({
   file: "//content.jwplatform.com/videos/kbs0WLBI-g8UjtXW6.m4a",
-  height: 40,
-  width: 480
 });
 
 player3.setup({
   file: "//content.jwplatform.com/videos/KJKBQC7h-g8UjtXW6.m4a",
-  height: 40,
-  width: 480
 });
 
 player1.on('play', function() {
