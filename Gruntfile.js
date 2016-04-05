c8035f5c36e738ee560cbe93ae29ec6087ebbcc2
Matthew Martindale <mmartindale@jwplayer.com>
2016-04-05 18:56:12 -0400

    major rework to use mustache vs preprocess for templating

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,106 +23,36 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-http-server');
-  grunt.loadNpmTasks('grunt-preprocess');
+  grunt.loadNpmTasks('grunt-mustache-render');
 
   // grunt default task
   grunt.registerTask('default', '', function() {
 
-    if (grunt.file.isDir('tmp')) grunt.file.delete('tmp');
-
-    const SRC = 'demos/';
-    const BUILD = 'build/';
-    const TMP = 'tmp/';
-    const TMPL = '_templates/';
-
+    // config vars
     var categories = grunt.config('config.categories'),
-      demos = [],
-      concatJS = {},
-      concatCSS = {},
-      concatIndexes = {},
-      preprocess = {},
-      tasks = [
-        'clean',
-        'concat:js',
-        'concat:css'
-      ];
+      demos = {
+        'all': []
+      },
+      concat = {},
+      mustacheRender = [],
+      paths = {
+        root: '/',
+        css: 'developer.jwplayer.com/css',
+        js: 'developer.jwplayer.com/js',
+        img: 'developer.jwplayer.com/img'
+      };
 
-    for (var i = 0; i < categories.length; i++) {
-
-      var cat = categories[i];
-
-      // if directory does not exist even despite being in category list
-      if (!grunt.file.isDir('demos/' + cat)) continue;
-
-      grunt.file.recurse('demos/' + cat,
-          function callback(abspath, rootdir, subdir, filename) {
-
-        // we are looking for a config file, which is require per demo
-        // when we find the config file, we process that directory as a demo
-        if (filename == 'config.json') {
-
-          // define demo-specific directory shortcuts
-          var srcDir = SRC + cat + '/' + subdir + '/';
-          var buildDir = BUILD + srcDir;
-          var tmpDir = TMP + srcDir;
-
-          // get demo config json
-          var demo = grunt.file.readJSON(srcDir + filename);
-
-          // append demo obj with demo category and directory
-          // then push demo to demos list to iterate over later
-          demo['category'] = cat;
-          demo['directory'] = subdir;
-          demos.push(demo);
-
-          // concat config for demo js
-          grunt.file.write(tmpDir + 'js/build.js', '');
-          concatJS[tmpDir + 'js/build.js'] = srcDir + 'js/*.js'
-
-          // concat config for demo css
-          grunt.file.write(buildDir + 'css/build.css', '');
-          concatCSS[buildDir + 'css/build.css'] = srcDir + 'css/*.css';
-
-          // preprocess config fir demo detail page
-          preprocess['detail-' + subdir] = {
-            options: {
-              context: {
-                default: '<%= grunt.file.read("' + TMPL + 'single.html") %>',
-                single: '<%= grunt.file.read("' + srcDir + 'index.html") %>',
-                js: '\r\r<%= grunt.file.read("' + tmpDir + 'js/build.js") %>\r',
-                category: demo.category,
-                directory: demo.directory,
-                title: demo.title,
-                desc: demo.description,
-                license: demo.license
-              }
-            },
-            files: [
-              {
-                src: TMPL + 'card.html',
-                dest: tmpDir + 'card.html'
-              },
-              {
-                src: TMPL + 'default.html',
-                dest: buildDir + 'index.html'
-              },
-              {
-                src: buildDir + 'index.html',
-                dest: buildDir + 'index.html'
-              }
-            ]
-          };
-
-          tasks.push('preprocess:detail-' + subdir);
-
-        }
-
-      });
-
+    // if `--dev-mode` option was passed to override configurable data, require
+    // a `local-config.json` file
+    if (grunt.option('dev-mode') && grunt.file.exists('local-config.json')) {
+      paths = grunt.file.readJSON('local-config.json').paths;
     }
 
-    // sort all demos alphabetically
-    demos.sort(function (a, b) {
+    // if `--prod` option was passed to specify build type
+    if (grunt.option('prod')) paths.root = '/jw-player/demos/'
+
+    // sort array/object alphabetically on the `directory` property
+    function sortABC(a, b) {
       var prop = 'directory';
       var sortStatus = 0;
       if (a[prop] < b[prop]) {
@@ -131,92 +61,143 @@ module.exports = function (grunt) {
         sortStatus = 1;
       }
       return sortStatus;
+    }
+
+    // loop categories and compile index and single pages for each child demo
+    for (var i = 0; i < categories.length; i++) {
+
+      // store category data in namespace
+      var cat = categories[i];
+
+      // namespace for category demo list
+      demos[cat.directory] = [];
+
+      // if directory does not exist even despite being in category list
+      if (!grunt.file.isDir('demos/' + cat.directory)) continue;
+
+      // scan category sub-directories to locate valid demos
+      grunt.file.recurse('demos/' + cat.directory,
+          function callback(abspath, rootdir, subdir, filename) {
+
+        // we are looking for a config file, which is require per demo
+        // when we find the config file, we process that directory as a demo
+        if (filename == 'config.json') {
+
+          // define demo-specific directory shortcuts
+          var srcDir = 'demos/' + cat.directory + '/' + subdir + '/';
+          var buildDir = 'build/' + srcDir;
+
+          // get demo config json
+          var demo = grunt.file.readJSON(srcDir + filename);
+
+          // append demo obj with demo category and directory
+          demo['category'] = cat.directory;
+          demo['directory'] = subdir;
+
+          // push demo data to both category and master demo list
+          demos[cat.directory].push(demo);
+          demos['all'].push(demo);
+
+          // concat config for demo js
+          grunt.file.write(buildDir + 'js/build.js', '');
+          concat[buildDir + 'js/build.js'] = srcDir + 'js/*.js'
+
+          // concat config for demo css
+          grunt.file.write(buildDir + 'css/build.css', '');
+          concat[buildDir + 'css/build.css'] = srcDir + 'css/*.css';
+
+          // mustache config for demo detail page
+          mustacheRender.push({
+            data: {
+              paths: paths,
+              html: '<%= grunt.file.read("' + srcDir + 'index.html") %>',
+              js: '\r\r<%= grunt.file.read("' + buildDir + 'js/build.js") %>\r',
+              css: buildDir + 'css/build.css',
+              category: demo.category,
+              directory: demo.directory,
+              title: demo.title,
+              description: demo.description,
+              license: demo.license,
+              showCode: function() {
+                return typeof demo.showCode !== 'undefined') ? demo.showCode : true;
+              },
+              horizontal: function() {
+                return typeof demo.layout === 'undefined' || demo.layout == 'horizontal';
+              },
+              vertical: function() {
+                return typeof demo.layout !== 'undefined' && demo.layout == 'vertical';
+              }
+            },
+            template: '_templates/single.mustache',
+            dest: buildDir + 'index.html'
+          });
+
+        }
+
+      });
+
+      // sort demos within this category
+      demos[cat.directory].sort(sortABC);
+
+      // mustache config for category demo index
+      mustacheRender.push({
+        data: {
+          paths: paths,
+          name: cat.name,
+          directory: cat.directory,
+          demos: demos[cat.directory]
+        },
+        template: '_templates/index.mustache',
+        dest: 'build/demos/' + cat.directory + '/index.html'
+      });
+
+    }
+
+    // sort complete list of demos
+    demos.all.sort(sortABC);
+
+    // mustache config for complete demo index
+    mustacheRender.push({
+      data: {
+        paths: paths,
+        name: 'All',
+        directory: '',
+        demos: demos.all
+      },
+      template: '_templates/index.mustache',
+      dest: 'build/demos/index.html'
     });
 
-    // add concat config namespaces for index pages
-    grunt.file.write(TMP + SRC + 'index.html', '');
-    concatIndexes[TMP + SRC + 'index.html'] = [];
-    for (var i = 0; i < categories.length; i++) {
-      grunt.file.write(TMP + SRC + categories[i] + '/index.html', '');
-      concatIndexes[TMP + SRC + categories[i] + '/index.html'] = [];
-    }
-
-    // loop demos and add them to concat src file list config for full index
-    // and category index it belongs to
-    for (var i = 0; i < demos.length; i++) {
-      var demo = demos[i];
-      concatIndexes[TMP + SRC + 'index.html'].push(
-        TMP + SRC + demo.category + '/' + demo.directory + '/card.html');
-      concatIndexes[TMP + SRC + demo.category + '/index.html'].push(
-        TMP + SRC + demo.category + '/' + demo.directory + '/card.html');
-    }
-
-    // add to task list
-    tasks.push('concat:indexes');
-
-    // preprocess config for full index page
-    preprocess['index-all'] = {
-      options: {
-        context: {
-          indexContent: '<%= grunt.file.read("' + TMP + SRC + 'index.html") %>',
-        }
-      },
-      files: [{
-        src: TMPL + 'index.html',
-        dest: BUILD + SRC + 'index.html'
-      }]
-    };
-
-    // add task to list
-    tasks.push('preprocess:index-all');
-
-    // preprocess config for each category index page
-    for (var i = 0; i < categories.length; i++) {
-
-      // define preprocess config
-      preprocess['index-' + categories[i]] = {
-        options: {
-          context: {
-            indexContent: '<%= grunt.file.read("' + TMP + SRC + categories[i] + '/index.html") %>',
-            selected: categories[i]
-          }
-        },
-        files: [{
-          src: TMPL + 'index.html',
-          dest: BUILD + SRC + categories[i] + '/index.html'
-        }]
-      };
-
-      // add task to list
-      tasks.push('preprocess:index-' + categories[i]);
-
-    }
-
     // set concat config
-    grunt.config.set('concat', {
-      js: {
-        files: concatJS
-      },
-      css: {
-        files: concatCSS
-      },
-      indexes: {
-        files: concatIndexes
+    grunt.config('concat', {
+      build: {
+        files: concat
       }
     });
 
-    // set preprocess config
-    grunt.config.set('preprocess', preprocess);
+    // set mustache config
+    grunt.config('mustache_render', {
+      options: {
+        directory: '_templates'
+      },
+      build: {
+        files: mustacheRender
+      }
+    });
 
     // run the task list
-    grunt.task.run(tasks);
+    grunt.task.run([
+      'clean',
+      'concat',
+      'mustache_render'
+    ]);
 
   });
 
   // build and serve locally
-  grunt.registerTask('serve', '', function() {
-    grunt.task.run('default');
-    grunt.task.run('http-server');
-  });
+  grunt.registerTask('serve', '', [
+    'default',
+    'http-server'
+  ]);
 
 };
