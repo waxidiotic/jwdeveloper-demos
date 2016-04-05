b2d896835e47b7272f40714be58b913483ef58af
Josie Keller <josie@JKeller-JWP.office.longtailvideo.com>
2016-04-05 16:48:32 -0400

    Restructured Closed Captions Demo

---
+++ demos/toolbox/closed-captions/js/main.js
@@ -0,0 +1,15 @@
+jwplayer("player1").setup({
+    file: 'http://wowzaec2demo.streamlock.net/vod-multitrack/_definst_/smil:ElephantsDream/elephantsdream2.smil/playlist.m3u8',
+
+    type: 'hls',
+        hlslabels: {
+        "415": "128p",
+        "515": "160p",
+        "650": "208p",
+        "881":"252p",
+        "1200":"332p",
+        "1600": "432p",
+        "2300": "560p",
+        "3200":"720p"
+        }
+    });
