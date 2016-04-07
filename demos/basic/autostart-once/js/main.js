53a41250d631f8a76711ab5181f69a67973e5b82
Matthew Martindale <madikarizma@gmail.com>
2016-04-07 09:30:10 -0400

    cleanup

--- demos/basic/autostart-once/js/main.js
+++ demos/basic/autostart-once/js/main.js
@@ -1,8 +1,8 @@
 player = jwplayer();
 
-player.on('ready', function(){
-  if(document.cookie.indexOf("jwplayerAutoStart") == -1) {
-    document.cookie = "jwplayerAutoStart=1";
+player.on('ready', function() {
+  if(document.cookie.indexOf('jwplayerAutoStart') == -1) {
+    document.cookie = 'jwplayerAutoStart=1';
     player.play();
   }
 });
