89a05ca05aa5a23be149b298f026b5d4b2508f71
Josie Keller <josie@JKeller-JWP.office.longtailvideo.com>
2016-05-23 15:34:37 -0400

    moving google ima demo to toolbox

--- demos/toolbox/mpeg-dash/js/main.js
+++ demos/toolbox/mpeg-dash/js/main.js
@@ -1,6 +1,6 @@
 jwplayer('player').setup({
   playlist:[{
-    file:"http://wowzaec2demo.streamlock.net/vod-multitrack/_definst_/smil:ElephantsDream/ElephantsDream.smil/manifest.mpd",
+    file:"https://wowzaec2demo.streamlock.net/vod-multitrack/_definst_/smil:ElephantsDream/ElephantsDream.smil/manifest.mpd",
     title:"Sintel",
     description:"This is a DASH stream!",
     type:"dash"
