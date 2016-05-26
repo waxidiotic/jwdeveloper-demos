8d3157a6fb4d23dcbfa44b84ab56f7ba54ea3036
Monica Parra <moni18r@gmail.com>
2016-05-26 11:44:48 -0400

    adding time formatting for duration

--- demos/customization/feeds-js/js/feeds_harness.js
+++ demos/customization/feeds-js/js/feeds_harness.js
@@ -1,10 +1,10 @@
-var jsWidget = (function(window,$,_,jwplayer) {
+var jsWidget = (function(window,$,_,jwplayer,jst) {
 
   var FEED_URL = '//content.jwplatform.com/feed.rss?feed_id=Xw0oaD4q&related_video=';
   var NUM_TILES = 5;
   //set up the page, including checking if there's params
   
-    function setup(playerdiv,tilediv) {
+    function _setup(playerdiv,tilediv) {
        var mediaid  = "uNXCVIsW";
         jwplayer(playerdiv).setup({
             file:"//content.jwplatform.com/videos/uNXCVIsW-8LSW5F2t.mp4",
@@ -28,11 +28,24 @@ var jsWidget = (function(window,$,_,jwplayer) {
               }
             });
         });
+
+        //format duration nicely
+        function tdur(s1) {
+          var h1 = Math.floor(s1/(60 * 60));
+          s1 %= 60 * 60;
+          var m1 = Math.floor(s1/60);
+          s1 %= 60;
+          var h2 = h1 ? h1+':' : '',
+              m2 = h1 && m1<10 ? '0'+m1 : m1,
+              s2 = s1<10 ? '0'+s1 : s1;
+          return h2 + m2 + ':' + s2;
+        }
         function getFeed(mediaid) {
             return FEED_URL + mediaid;
         }
         function updateTiles(data) {
             var $xml = $(data);
+            // use jwplayer rss parser to parse the Media RSS, then set up the player with the item on click
             var feed = jwplayer.utils.rssparser.parse($xml[0].children[0]);
 
             parent = $("#" + tilediv);
@@ -43,10 +56,10 @@ var jsWidget = (function(window,$,_,jwplayer) {
                   id: id,
                   title:item.title,
                   desc:item.description,
-                  dur:item.duration,
+                  dur:tdur(item.duration) || "",
                   image:item.image
                 };
-                var itemTemplate = JST['templates/item.hbs'];
+                var itemTemplate = jst['templates/item.hbs'];
                 var html = itemTemplate(data);
                 parent.append(html);
                 $("#"+id).click(function() {
@@ -56,11 +69,9 @@ var jsWidget = (function(window,$,_,jwplayer) {
 
             });
           }
-
-
     }
     return {
-        setup:setup
+        setup:_setup
     };
 
-})(window,$,_,window.jwplayer);
+})(window,$,_,window.jwplayer,JST);
