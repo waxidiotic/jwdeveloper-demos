5ce8bfc746a48b1a31bda5284277deae7a353022
Monica Parra <moni18r@gmail.com>
2016-06-09 13:09:47 -0400

    updating JS demo with JSON feed

--- demos/customization/feeds-js/dest/templates.js
+++ demos/customization/feeds-js/dest/templates.js
@@ -1,7 +1,7 @@
 this["JST"] = this["JST"] || {};
 
 this["JST"]["templates/error.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div id=\"error\" class=\"jw-feed-error\">\n  No Recommendations Found\n</div>";
+    return "<div id=\"error\">\n  <p>No Recommendations Found</p>\n</div>";
 },"useData":true});
 
 this["JST"]["templates/item.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
@@ -9,13 +9,13 @@ this["JST"]["templates/item.hbs"] = Handlebars.template({"compiler":[7,">= 4.0.0
 
   return "<li id="
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + " class=\"jw-option\">\n	<div class=\"jw-thumbnail-container\">\n		<img class=\"jw-thumbnail\" src=\""
+    + " class=\"jw-option\">\n  <div class=\"jw-thumbnail-container\">\n        <img class=\"jw-thumbnail\" src=\""
     + alias4(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data}) : helper)))
-    + "\"/>\n	</div>\n	<div class=\"jw-metadata-container\">\n		<h3 class=\"jw-media-title\">"
+    + "\"/>\n   </div>\n    <div class=\"jw-metadata-container\">\n     <h3 class=\"jw-media-title\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h3>\n		<!--<p class=\"jw-media-description\">"
+    + "</h3>\n      <!--<p class=\"jw-media-description\">"
     + alias4(((helper = (helper = helpers.desc || (depth0 != null ? depth0.desc : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"desc","hash":{},"data":data}) : helper)))
-    + "</p> -->\n		<time class=\"jw-media-duration\">"
+    + "</p> -->\n       <time class=\"jw-media-duration\">"
     + alias4(((helper = (helper = helpers.dur || (depth0 != null ? depth0.dur : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"dur","hash":{},"data":data}) : helper)))
-    + "</time>\n	</div>\n</li>";
+    + "</time>\n    </div>\n</li>";
 },"useData":true});
--- demos/customization/feeds-js/js/feeds_harness.js
+++ demos/customization/feeds-js/js/feeds_harness.js
@@ -1,87 +1,102 @@
 var jsWidget = (function(window,$,_,jwplayer,jst) {
 
-  // RSS feed - Default
-  var FEED_URL = '//content.jwplatform.com/feed.rss?feed_id=Xw0oaD4q&related_media_id=';
+  var JW_API = '//content.jwplatform.com';    
+  function _setup(playerdiv,tilediv, titlediv, media_id,feed_id) {
+    
+    function _init(media_id) {
+      $.ajax({
+        type: "GET",
+        url: [JW_API,'feeds',media_id + '.json'].join('/'),
+        processData: false,
+        dataType: "json",
+        success: _setupPlayer,
+        error: function (e) {
+            console.log("Error downloading feed");
+        },
+        fail: function (r) {
+            console.log("Error downloading feed");
+        }
+      });
+    }
 
-  // JSON feed - uncomment to use json instead of rss
-  //var FEED_URL = '//content.jwplatform.com/feed.json?feed_id=Xw0oaD4q&related_media_id=';
-
-
-  //set up the page, including checking if there's params
-  
-    function _setup(playerdiv,tilediv) {
-       var mediaid  = "uNXCVIsW";
-        jwplayer(playerdiv).setup({
-            file:"//content.jwplatform.com/videos/uNXCVIsW-8LSW5F2t.mp4",
-            image:"//content.jwplatform.com/thumbs/uNXCVIsW-480.jpg",
-            mediaid: mediaid
-
-
-        });
-        jwplayer(playerdiv).on('playlistItem', function(evt) {
-            $.ajax({
-              type: "GET",
-              url: getFeed(evt.item.mediaid),
-              processData: false,
-              dataType: "xml",
-              success: updateTiles,
-              error: function (e) {
-                  console.log("Error downloading feed");
-              },
-              fail: function (r) {
-                  console.log("Error downloading feed");
-              }
+    function _setupPlayer(item) {
+      jwplayer(playerdiv).setup(item);
+      jwplayer(playerdiv).on('playlistItem', function(evt) {
+          $.ajax({
+            type: "GET",
+            url: _getFeed(evt.item.mediaid),
+            processData: false,
+            dataType: "json",
+            success: _updateTiles,
+            error: function (e) {
+                console.log("Error downloading feed");
+            },
+            fail: function (r) {
+                console.log("Error downloading feed");
+            }
+          });
+      });
+    }
+    //time formatting for media duration
+    function _tdur(s1) {
+      if (s1 === "") return s1;
+      var h1 = Math.floor(s1/(60 * 60));
+      s1 %= 60 * 60;
+      var m1 = Math.floor(s1/60);
+      s1 %= 60;
+      var h2 = h1 ? h1+':' : '',
+          m2 = h1 && m1<10 ? '0'+m1 : m1,
+          s2 = s1<10 ? '0'+s1 : s1;
+      return h2 + m2 + ':' + s2;
+    }
+    function _getFeed(media_id) {
+      return [JW_API,"feed.json?"+ _parameterize({feed_id: feed_id,related_video:media_id})].join("/");
+    }
+    function _parameterize(params) {
+      return _.reduce(params,function(memo, key, val) { 
+          memo.push(val + '=' + encodeURIComponent(key)); 
+          return memo;
+        },
+        []
+        ).join('&');
+    }
+    function _updateTiles(data) {
+     //to use rss feed:
+     //var $xml = $(data);
+     // use jwplayer rss parser to parse the Media RSS, then set up the player with the item on click
+     //data= jwplayer.utils.rssparser.parse($xml[0].children[0]);
+     // $("#" + titlediv).html($xml.find('title').first().text());
+      $("#" + titlediv).html(data.title);
+      parent = $("#" + tilediv);
+      parent.empty();
+      if (_.size(data.playlist) === 0) {
+        _addTemplate(jst['templates/error.hbs'], {}, parent);
+      } else {
+        _.each(data.playlist,function(item,i) {
+            var id = "item" + i;
+            var templateData = {
+              id: id,
+              title:item.title,
+              desc:item.description,
+              dur:_tdur(item.sources[0].duration || ""),
+              image:item.image
+            };
+            _addTemplate(jst['templates/item.hbs'], templateData, parent);
+            $("#"+id).click(function() {
+              jwplayer(playerdiv).load(item);
             });
         });
-
-        //time formatting for media duration
-        function tdur(s1) {
-          var h1 = Math.floor(s1/(60 * 60));
-          s1 %= 60 * 60;
-          var m1 = Math.floor(s1/60);
-          s1 %= 60;
-          var h2 = h1 ? h1+':' : '',
-              m2 = h1 && m1<10 ? '0'+m1 : m1,
-              s2 = s1<10 ? '0'+s1 : s1;
-          return h2 + m2 + ':' + s2;
-        }
-        function getFeed(mediaid) {
-            return FEED_URL + mediaid;
-        }
-        function updateTiles(data) {
-            var $xml = $(data);
-            // use jwplayer rss parser to parse the Media RSS, then set up the player with the item on click
-            var feed = jwplayer.utils.rssparser.parse($xml[0].children[0]);
-
-            parent = $("#" + tilediv);
-            parent.empty();
-            // if feed fails to load, display error message
-            if (_.size(feed) === 0) {
-              addTemplate(jst['templates/error.hbs'], {}, parent);
-            } else {
-              _.each(feed,function(item,i) {
-                  var id = "item" + i;
-                  var templateData = {
-                    id: id,
-                    title:item.title,
-                    desc:item.description,
-                    dur:tdur(item.duration) || "",
-                    image:item.image
-                  };
-                  addTemplate(jst['templates/item.hbs'], templateData, parent);
-                  $("#"+id).click(function() {
-                    jwplayer(playerdiv).load(item);
-                  });
-              });
-            }
-          }
-          function addTemplate(itemTemplate,data,parent) {
-            var html = itemTemplate(data);
-            parent.append(html);
-          }
+      }
     }
-    return {
-        setup:_setup
-    };
+    function _addTemplate(itemTemplate,data,parent) {
+      var html = itemTemplate(data);
+      parent.append(html);
+    }
+    _init(media_id);
+  }
+
+  return {
+      setup:_setup
+  };
 
 })(window,$,_,window.jwplayer,JST);
