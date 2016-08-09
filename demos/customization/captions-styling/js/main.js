1c4b19696b2542d5d1daf4c90437768e2f92fc22
Andrew Prelusky <andrewgprelusky@gmail.com>
2016-08-09 17:54:21 -0400

    Updated demo to place assets in correct spots, place code in correct file, and added APIs to display captions at a correct time

--- demos/customization/captions-styling/js/main.js
+++ demos/customization/captions-styling/js/main.js
@@ -1,22 +1,17 @@
-<script>
-var playerInstance = jwplayer("container");
 var styleObject= {};
-
-playerInstance.setup({
-	"file": "http://content.jwplatform.com/videos/1g8jjku3-cIp6U8lV.mp4",
-	"tracks": [{
-		file: "assets/captions.vtt",
-		type: 'captions'
-	}]
-
-});
-
 
 function update(style, value) {
 	console.log(style +", "+ value)
 	styleObject[style] = value;
     jwplayer().setCaptions(styleObject);
-    //document.getElementById("previewText").style.background = value;
 }
 
-</script>
+jwplayer().on('ready', function(event){
+jwplayer().play(true);
+});
+
+jwplayer().on('firstFrame', function(event){
+jwplayer().seek(1);
+jwplayer().setCurrentCaptions(1)
+jwplayer().pause(true);
+});
