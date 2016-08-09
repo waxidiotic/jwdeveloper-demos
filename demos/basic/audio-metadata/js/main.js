01904cdd5733aad704538271149a175b881ef422
Josie Keller <josie@JKeller-JWP.office.longtailvideo.com>
2016-08-09 14:47:25 -0400

    updated description

--- demos/basic/audio-metadata/js/main.js
+++ demos/basic/audio-metadata/js/main.js
@@ -3,18 +3,18 @@ var artistHTML = document.getElementById('artist');
 var imageHTML = document.getElementById('image');
 
 jwplayer().on('meta', function(event){
-var thisTitle = event.metadata.title;
-var thisArtist = event.metadata.artist;
-var thisImage = event.metadata.url;
-console.log(event.metadata)
+	var thisTitle = event.metadata.title;
+	var thisArtist = event.metadata.artist;
+	var thisImage = event.metadata.url;
+	console.log(event.metadata)
 
-if(thisTitle){
-titleHTML.innerHTML= thisTitle;
-}
-if(thisArtist){
-artistHTML.innerHTML= thisArtist;
-}
-if(thisImage){
-imageHTML.innerHTML= '<img src="'+thisImage+'"/>';
-}
+	if(thisTitle){
+	titleHTML.innerHTML= thisTitle;
+	}
+	if(thisArtist){
+	artistHTML.innerHTML= thisArtist;
+	}
+	if(thisImage){
+	imageHTML.innerHTML= '<img src="'+thisImage+'"/>';
+	}
 });
