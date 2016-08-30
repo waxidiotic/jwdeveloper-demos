18b1ed969ccf534e14a18706d234bc425167712f
Cooper Reid <cooper@coopers-mbp.office.longtailvideo.com>
2016-08-30 17:35:59 -0400

    fixed fcn measuring element absolute position.

--- demos/advanced/outstream-advertising/js/main.js
+++ demos/advanced/outstream-advertising/js/main.js
@@ -1,10 +1,17 @@
-var playerInstance = jwplayer('player'),
-    wrapper = document.querySelector('#player-wrapper');
-
 /**
 * Some utility fcn's for listening for `video-in-view`
 */
-var _bind = function(el, evt, fcn) {
+var playerInstance = jwplayer('player'),
+    wrapper = document.querySelector('#player-wrapper'),    
+    scrollPlayed = false,
+    playerHeight,
+    checker = null,
+    elementPos = function(element) {
+        var bodyRect = document.body.getBoundingClientRect(),
+            elemRect = element.getBoundingClientRect();
+        return elemRect.top - bodyRect.top;
+    }, 
+    _bind = function(el, evt, fcn) {
         el[window.addEventListener ? 'addEventListener' : 'attachEvent']( window.addEventListener ? evt : 'on' + evt, fcn, false);
     },
     scrollPos = function() {
@@ -13,15 +20,14 @@ var _bind = function(el, evt, fcn) {
             'left': (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0), 
             'top': (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0)
         };
-    },    
-    playerOffset = wrapper.offsetTop,
-    scrollPlayed = false,
+    },     
     playerInView = function() {
         var diff = playerOffset - scrollPos().top,
-            adjust = window.innerHeight > (480/2) ? window.innerHeight : 100;
-        return diff > -100 && diff < adjust;        
+            adjust = window.innerHeight > (playerHeight/2) ? window.innerHeight : 100,
+            inView = diff > -100 && diff < adjust;
+        return inView;        
     },
-    checker = null;    
+    playerOffset = elementPos(wrapper);
 
 /**
 * Setup the player
@@ -34,21 +40,25 @@ playerInstance.setup({
     }
 });
 
-/**
-* Bind the scroll event to check for `video-in-view`
-*/
-_bind(window, 'scroll', function(e) {
-    clearTimeout(checker);
-    if(playerInView()) {
-        wrapper.style.height = '270px';
-        checker = setTimeout(function() {
-            playerInstance.play(true);
-            scrollPlayed = true;            
-        }, 100);
-    }    
-    else if(!playerInView()) {
-        playerInstance.pause(true);
-    }
+playerInstance.on('ready', function() {
+    // get player height once ready
+    playerHeight = playerInstance.getHeight(); 
+    /**
+    * Bind the scroll event to check for `video-in-view`
+    */
+    _bind(window, 'scroll', function(e) {
+        clearTimeout(checker);
+        if(playerInView()) {
+            wrapper.style.height = playerHeight + 'px';
+            checker = setTimeout(function() {
+                playerInstance.play(true);
+                scrollPlayed = true;            
+            }, 100);
+        }    
+        else {
+            playerInstance.pause(true);
+        }
+    });    
 });
 
 /**
