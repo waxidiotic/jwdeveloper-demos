c2a5879d5097b6a061353173f33618337ecfc1e9
Jean-Baptiste Louazel <jb.louazel@gmail.com>
2016-09-01 16:27:32 +0200

    Update main file (new stream and streamrootKey)

--- demos/innovation/peer-accelerated-delivery/js/main.js
+++ demos/innovation/peer-accelerated-delivery/js/main.js
@@ -1,31 +1,27 @@
-var init = function () {
+(function () {
+	'use strict';
 
-     jwplayer('streamroot-player')
-     .setup({
-         playlist : [{
-             file: "https://wowza.streamroot.io/liveorigin/stream4/playlist.m3u8"
-         }],
-         p2pConfig:{
-             streamrootKey: "sr-ewok",
-             debug: true,
-         },
-         hlsjsConfig : {
-            liveSyncDuration: 40, 
-            liveMaxLatencyDuration: 80
-         },
-        logo: {
-            file: './assets/logo.png',
-            link: 'https://streamroot.io',
-            position :'top-left'
-        },
-         width: 600,
-         height: 400,
-         autostart: true
-     });
+	jwplayer('streamroot-player')
+		.setup({
+			playlist: [{
+				file: 'https://wowza-cloudfront.streamroot.io/liveorigin/stream4/playlist.m3u8'
+			}],
+			p2pConfig: {
+				streamrootKey: 'demoswebsiteandpartners',
+				debug: true
+			},
+			hlsjsConfig: {
+				liveSyncDuration: 40,
+				liveMaxLatencyDuration: 80
+			},
+			logo: {
+				file: './assets/logo.png',
+				link: 'https://streamroot.io',
+				position: 'top-left'
+			},
+			width: 600,
+			height: 400,
+			autostart: true
+		});
 
- };
-
- init();
-
-
-
+})();
