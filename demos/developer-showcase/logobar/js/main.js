bc24b7125eb54e8a17c60eacc9dd09e7d6480f68
Monica Parra <moni18r@gmail.com>
2016-12-01 15:46:57 -0500

    styling for logotv demo

--- demos/developer-showcase/logobar/js/main.js
+++ demos/developer-showcase/logobar/js/main.js
@@ -1,2 +1,1 @@
-var lbvast = ["https://vast.logobar.tv/lbvast.php?cid=90kre6s5c66m"];</script>
-<script src="https://cdn2.logobar.tv/plugin/jw/lbplugin4jw.min.js">
+	var lbvast = ["https://vast.logobar.tv/lbvast.php?cid=90kre6s5c66m"];
