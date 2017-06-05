1bb3e2c9fe9a1b34a7984dda2c8f54ca3263e117
Joshua Hatcher <joshua@jwplayer.com>
2017-06-05 11:27:31 -0400

    WEB-559 Move button hrefs to click events in JS, minor styling, add demo description

--- demos/basic/chromeless-player/js/main.js
+++ demos/basic/chromeless-player/js/main.js
@@ -5,3 +5,14 @@ player.setup({
     image: '//content.jwplatform.com/thumbs/jumBvHdL-720.jpg',
     controls: false
 });
+
+var playBtn = document.getElementById('play');
+var pauseBtn = document.getElementById('pause');
+
+playBtn.addEventListener('click', function() {
+  player.play(true);
+});
+
+pauseBtn.addEventListener('click', function() {
+  player.pause(true);
+});
