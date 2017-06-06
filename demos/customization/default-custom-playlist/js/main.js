7c200e6c71cf22b08b05613b83033179f8f3eb4e
Joshua Hatcher <joshua@jwplayer.com>
2017-06-06 16:15:19 -0400

    WEB-548 Remove unnecessary self invoking fn and use strict

--- demos/customization/default-custom-playlist/js/main.js
+++ demos/customization/default-custom-playlist/js/main.js
@@ -1,67 +1,62 @@
-(function() {  
-  'use strict';  
+// Start Playlist Overlay Example
+function initInPlayer(playlistId) {
+  jwplayer('in-player-demo').setup({
+    displaytitle: true,
+    logo: false,
+    playlist: 'https://cdn.jwplayer.com/v2/playlists/' + playlistId,
+    visualplaylist: true
+  });
+};
 
-  // Start Playlist Overlay Example
-  function initInPlayer(playlistId) {
-    jwplayer('in-player-demo').setup({
-      displaytitle: true,
+initInPlayer('6tYY3mSy');
+// End Playlist Overlay Example
+
+// Start Playlist Shelf Example
+function initOutPlayer(playlistId) {
+  var playerInstance = jwplayer('out-player-demo')
+    .setup({
+      displaytitle: false,
       logo: false,
+      autostart: false,
       playlist: 'https://cdn.jwplayer.com/v2/playlists/' + playlistId,
-      visualplaylist: true
-    });
-  };
-
-  initInPlayer('6tYY3mSy');
-  // End Playlist Overlay Example
-
-  // Start Playlist Shelf Example
-  function initOutPlayer(playlistId) {
-    var playerInstance = jwplayer('out-player-demo')
-      .setup({
-        displaytitle: false,
-        logo: false,
-        autostart: false,
-        playlist: 'https://cdn.jwplayer.com/v2/playlists/' + playlistId,
-        width: '100%',
-        aspectratio: '16:9'
-      });
-
-    initPlaylist(playerInstance);
-  }
-
-  function initPlaylist(player) {
-    $.ajax(player.getPlaylist()).then(function(data) {
-      var playlistWrapper = $('.playlist-wrapper');
-      var playlistTemplate = Handlebars.compile($('#out-player-playlist-template').html());
-      playlistWrapper.html(playlistTemplate(data));
-
-      playlistWrapper.on('click', '.playlist-item-link', setVideo.bind(this, data.playlist, player));
-      player.on('playlistItem', setActive);
-    });
-  }
-
-  function setVideo(playlist, player, e) {
-    var currentVideo = $(e.target).closest('a').data('mediaid');
-    var videoIndex = playlist.findIndex(function(video) {
-      return currentVideo === video.mediaid;
+      width: '100%',
+      aspectratio: '16:9'
     });
 
-    e.preventDefault();
+  initPlaylist(playerInstance);
+}
 
-    player.playlistItem(videoIndex);
-  }
+function initPlaylist(player) {
+  $.ajax(player.getPlaylist()).then(function(data) {
+    var playlistWrapper = $('.playlist-wrapper');
+    var playlistTemplate = Handlebars.compile($('#out-player-playlist-template').html());
+    playlistWrapper.html(playlistTemplate(data));
 
-  function setActive(e) {
-    var id = e.item.mediaid;
+    playlistWrapper.on('click', '.playlist-item-link', setVideo.bind(this, data.playlist, player));
+    player.on('playlistItem', setActive);
+  });
+}
 
-    $('.playlist-item-link').removeClass('active')
-      .filter(function(item) {
-        return $(this).data('mediaid') === id;
-      })
-      .addClass('active');
-  }
+function setVideo(playlist, player, e) {
+  var currentVideo = $(e.target).closest('a').data('mediaid');
+  var videoIndex = playlist.findIndex(function(video) {
+    return currentVideo === video.mediaid;
+  });
 
-  initOutPlayer('6tYY3mSy');
-  // End Playlist Shelf Example
+  e.preventDefault();
 
-})();
+  player.playlistItem(videoIndex);
+}
+
+function setActive(e) {
+  var id = e.item.mediaid;
+
+  $('.playlist-item-link').removeClass('active')
+    .filter(function(item) {
+      return $(this).data('mediaid') === id;
+    })
+    .addClass('active');
+}
+
+initOutPlayer('6tYY3mSy');
+// End Playlist Shelf Example
