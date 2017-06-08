bab9cc7e5a7177b07e46536de6bf2d77eab1c8fe
Joshua Hatcher <joshua@jwplayer.com>
2017-06-08 10:59:14 -0400

    WEB-574 Begin framework for new playback rate demo

---
+++ demos/advanced/set-playback-rate/js/main.js
@@ -0,0 +1,6 @@
+jwplayer('player').setup({
+  mediaid: 'gaCRFWjn',
+  file: '//content.jwplatform.com/videos/gaCRFWjn-Zq6530MP.mp4',
+  playbackRateControls: [0.75, 1, 1.25, 1.5],
+  asdga: 'asgah'
+});
