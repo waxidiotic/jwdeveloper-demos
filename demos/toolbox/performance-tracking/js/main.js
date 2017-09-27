aad0248628671d9dbc6db3532333b438cae13e74
kimhart <kim@jwplayer.com>
2017-09-27 17:01:07 -0400

    page is reloading on toggle between the two versions

--- demos/toolbox/performance-tracking/js/main.js
+++ demos/toolbox/performance-tracking/js/main.js
@@ -1,28 +1,27 @@
 var logs = jwTest.makeLogger('container');
 
 function prettify(q, event) {
-    if (!q.item.counts[event] && !q.item.sums[event]) {
-        return event + ' (undefined)';
-    }
-    return event + ' ('+q.item.counts[event]+') ' + q.item.sums[event];
+  if (!q.item.counts[event] && !q.item.sums[event]) {
+      return event + ' (undefined)';
+  }
+  return event + ' ('+q.item.counts[event]+') ' + q.item.sums[event];
 }
 
 var player = jwplayer('container');
 
 player.setup({
-    displaytitle: false,
-    preload:'metadata',
-    file: '//content.jwplatform.com/videos/xJ7Wcodt-FctPAkow.mp4',
-    image:'//content.jwplatform.com/thumbs/xJ7Wcodt-1280.jpg'
+  displaytitle: false,
+  preload:'metadata',
+  file: '//content.jwplatform.com/videos/i3q4gcBi.mp4',
+  image:'//content.jwplatform.com/thumbs/i3q4gcBi.jpg'
 });
 
-player.on('ready', function(){
-    var qoe = this.qoe();
-    logs.log('The player setup in:', JSON.stringify(qoe.setupTime),'ms');
-
+player.on('ready', function() {
+  var qoe = this.qoe();
+  logs.log('The player setup in:', JSON.stringify(qoe.setupTime),'ms');
 });
 
-player.on('firstFrame', function(){
-    var qoe = this.qoe();
-    logs.log('The player took', JSON.stringify(qoe.firstFrame),'ms to get to the first frame of video.');
+player.on('firstFrame', function() {
+  var qoe = this.qoe();
+  logs.log('The player took', JSON.stringify(qoe.firstFrame),'ms to get to the first frame of video.');
 });
