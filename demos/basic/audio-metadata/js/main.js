89c7c220177c7893be99ca97c6439a1f0d5d804e
Josie Keller <josannahkeller@gmail.com>
2017-10-11 11:49:14 -0400

    updating player in audio metadata demo

--- demos/basic/audio-metadata/js/main.js
+++ demos/basic/audio-metadata/js/main.js
@@ -2,19 +2,26 @@ var titleHTML = document.getElementById('title');
 var artistHTML = document.getElementById('artist');
 var imageHTML = document.getElementById('image');
 
-jwplayer().on('meta', function(event){
+// Setting up the player with an HLS stream that includes timed metadata
+var playerInstance = jwplayer("metaData");
+playerInstance.setup({
+	file: 'assets/index.m3u8',
+});
+
+// Retrieving metadata
+playerInstance.on('meta', function(event){
 	var thisTitle = event.metadata.title;
 	var thisArtist = event.metadata.artist;
 	var thisImage = event.metadata.url;
-	console.log(event.metadata)
+	console.log(event.metadata);
 
 	if(thisTitle){
 	titleHTML.innerHTML= thisTitle;
-	}
+		}
 	if(thisArtist){
 	artistHTML.innerHTML= thisArtist;
-	}
+		}
 	if(thisImage){
 	imageHTML.innerHTML= '<img src="'+thisImage+'"/>';
-	}
+		}
 });
