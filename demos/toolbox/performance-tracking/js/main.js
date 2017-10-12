6160047c14f4434155b8ccbbc2f2a29efa1a7a1c
Joshua Hatcher <joshua@jwplayer.com>
2017-10-12 12:06:13 -0400

    [WEB-687] Fix demo by delaying init of bulk of code until jwplayer global var exists on page.

--- demos/toolbox/performance-tracking/js/main.js
+++ demos/toolbox/performance-tracking/js/main.js
@@ -1,27 +1,31 @@
-var logs = jwTest.makeLogger('container');
+function init() {
+  var player, logs;
 
-function prettify(q, event) {
-  if (!q.item.counts[event] && !q.item.sums[event]) {
-      return event + ' (undefined)';
+  // For demo purposes
+  if (typeof jwplayer === 'undefined') {
+    setTimeout(init, 100);
+    return;
   }
-  return event + ' ('+q.item.counts[event]+') ' + q.item.sums[event];
+
+  player = jwplayer('container');
+  logs = jwTest.makeLogger('container');
+
+  player.setup({
+    displaytitle: false,
+    preload:'metadata',
+    file: '//content.jwplatform.com/videos/i3q4gcBi.mp4',
+    image:'//content.jwplatform.com/thumbs/i3q4gcBi.jpg'
+  });
+
+  player.on('ready', function() {
+    var qoe = this.qoe();
+    logs.log('The player set up in', JSON.stringify(qoe.setupTime),'ms.');
+  });
+
+  player.on('firstFrame', function() {
+    var qoe = this.qoe();
+    logs.log('The player took', JSON.stringify(qoe.firstFrame),'ms to get to the first frame of video.');
+  });
 }
 
-var player = jwplayer('container');
-
-player.setup({
-  displaytitle: false,
-  preload:'metadata',
-  file: '//content.jwplatform.com/videos/i3q4gcBi.mp4',
-  image:'//content.jwplatform.com/thumbs/i3q4gcBi.jpg'
-});
-
-player.on('ready', function() {
-  var qoe = this.qoe();
-  logs.log('The player set up in', JSON.stringify(qoe.setupTime),'ms.');
-});
-
-player.on('firstFrame', function() {
-  var qoe = this.qoe();
-  logs.log('The player took', JSON.stringify(qoe.firstFrame),'ms to get to the first frame of video.');
-});
+init();
