2393e5f501bc436a07e8a2293f3d001d3bed1302
kimhart <kim@jwplayer.com>
2017-10-19 10:27:34 -0400

    minor syntax/copy tweaks

--- demos/advanced/dynamic-ad-playback/js/main.js
+++ demos/advanced/dynamic-ad-playback/js/main.js
@@ -3,44 +3,44 @@
   var player = jwplayer('player');
 
   player.setup({
-    playlist: 'https://cdn.jwplayer.com/v2/media/hWF9vG66',
+    playlist: '//cdn.jwplayer.com/v2/media/hWF9vG66',
     advertising: {
       client: 'vast'
     }
   });
 
   player.on('play',function() {
-    addButton();
+    showButton();
   });
 
   player.on('pause',function() {
-    removeButton("Un-pause the video to play an ad.");
+    hideButton('Unpause the video to continue.');
   });
 
   player.on('complete', function() {
-    removeButton("Restart video to play an ad.");
+    hideButton('Restart video to continue.');
   });
 
   player.on('adComplete', function() {
-    addButton();
+    showButton();
   });
 
   player.on('adSkipped', function() {
-    addButton();
+    showButton();
   });
 
   function playDynamicAd() {
     player.playAd(tag);
-    removeButton("Ad playing, please wait...");
+    hideButton('Ad playing, please wait (or skip it)');
   };
 
-  function addButton() {
+  function showButton() {
     message.innerHTML = 'Play an Ad';
     message.classList.add('button');
     message.addEventListener('click', playDynamicAd);
   };
 
-  function removeButton(messageText) {
+  function hideButton(messageText) {
     message.innerHTML = messageText;
     message.classList.remove('button');
     message.removeEventListener('click', playDynamicAd);
