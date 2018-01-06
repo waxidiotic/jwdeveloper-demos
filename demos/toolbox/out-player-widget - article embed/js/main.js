e78d88a7631a6b69387707e2186f28a9206f2720
Monica Parra <moni18r@gmail.com>
2018-01-06 15:57:34 -0500

    adding 2 out-player widget demos under toolbox category

---
+++ demos/toolbox/out-player-widget - article embed/js/main.js
@@ -0,0 +1,9 @@
+jwplayer('widget-video-player').setup({
+  playlist: 'https://cdn.jwplayer.com/v2/media/RDn7eg0o',
+  autostart: false
+});
+
+jwplayer('ads-video-player').setup({
+  playlist: 'https://cdn.jwplayer.com/v2/media/z0nRtVrT',
+  autostart: false
+});
--- demos/toolbox/out-player-widget/js/main.js
+++
@@ -1,7 +0,0 @@
-jwplayer('widget-video-player').setup({
-  playlist: 'https://cdn.jwplayer.com/v2/media/tkM1zvBq',
-  autostart: false,
-  width: '780px',
-  height: '420px'
-});
-
